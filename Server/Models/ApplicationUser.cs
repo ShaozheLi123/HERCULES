013c951cb046935f8f83b7778ac10abe3142f182
shaozhe <lsz@udel.edu>
2022-11-05 17:13:47 -0400

    Final change

--- Server/Models/ApplicationUser.cs
+++ Server/Models/ApplicationUser.cs
@@ -1,7 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace HERCULES.Server.Models;
-
-public class ApplicationUser : IdentityUser
+namespace HERCULES.Server.Models
 {
+    public class ApplicationUser: IdentityUser
+    {
+    }
 }
+
